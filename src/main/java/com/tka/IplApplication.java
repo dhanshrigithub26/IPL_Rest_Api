7583c4ff6ca8b03ee5bf4d342f8f9fa83c286d52
PBL project <nemadedhanshri2005@gmail.com>
2025-01-12 13:47:19 +0530

    Second commit

--- src/main/java/com/tka/IplApplication.java
+++ src/main/java/com/tka/IplApplication.java
@@ -3,11 +3,14 @@ package com.tka;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import ch.qos.logback.core.net.SyslogOutputStream;
+
 @SpringBootApplication
 public class IplApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(IplApplication.class, args);
+		System.out.println("true");
 	}
 
 }
